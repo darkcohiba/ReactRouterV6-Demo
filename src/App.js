370017706f7dcb4d01c9bb6e45148261d5983943
Sam <samuel.waters@gmail.com>
2022-10-24 16:36:22 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/contact" element={<Contact />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+{/* <Router>
+<Routes>
+  <Route path= "/"  element = {<Homepage isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user}/>} />
+  <Route path= "/login"  element = {<Login isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} />} />
+  <Route path= "/Auth"  element = {<Auth isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} />} />
+  <Route path= "/Map"  element = {<MapPage isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} />} />
+  <Route path= "/AddPost"  element = {<Protected isAuthenticated={isAuthenticated}><AddPost bottleID={bottleID} setBottleID={setBottleID} postID={postID} setPostID={setPostID} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} /></Protected>} />
+  <Route path= "/UserPosts"  element = {<Protected isAuthenticated={isAuthenticated}><UserPosts bottleID={bottleID} setBottleID={setBottleID} postID={postID} setPostID={setPostID} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} /></Protected>} />
+  <Route path= "/AddBottle"  element = {<AddBottle bottleID={bottleID} setBottleID={setBottleID} postID={postID} setPostID={setPostID} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} />} />
+  <Route path= "/AddAddress"  element = {<AddAddress bottleID={bottleID} setBottleID={setBottleID} postID={postID} setPostID={setPostID} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} />} />
+</Routes>
+</Router> */}
