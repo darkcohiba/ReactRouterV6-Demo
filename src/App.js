eb8b69c74d754aa86e8fe8b39b480c0461673986
Sam <samuel.waters@gmail.com>
2022-10-25 10:25:40 -0600

    first demo

--- src/App.js
+++ src/App.js
@@ -23,15 +23,38 @@ function App() {
 export default App;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 {/* <Router>
 <Routes>
-  <Route path= "/"  element = {<Homepage isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user}/>} />
-  <Route path= "/login"  element = {<Login isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} />} />
-  <Route path= "/Auth"  element = {<Auth isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} />} />
-  <Route path= "/Map"  element = {<MapPage isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} />} />
-  <Route path= "/AddPost"  element = {<Protected isAuthenticated={isAuthenticated}><AddPost bottleID={bottleID} setBottleID={setBottleID} postID={postID} setPostID={setPostID} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} /></Protected>} />
-  <Route path= "/UserPosts"  element = {<Protected isAuthenticated={isAuthenticated}><UserPosts bottleID={bottleID} setBottleID={setBottleID} postID={postID} setPostID={setPostID} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} /></Protected>} />
-  <Route path= "/AddBottle"  element = {<AddBottle bottleID={bottleID} setBottleID={setBottleID} postID={postID} setPostID={setPostID} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} />} />
-  <Route path= "/AddAddress"  element = {<AddAddress bottleID={bottleID} setBottleID={setBottleID} postID={postID} setPostID={setPostID} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} user={user} />} />
+  <Route path="/" element={<Home />}/>
+  <Route path="/about" element={<About />}/>
+  <Route path="/contact" element={<Contact />}/>
 </Routes>
 </Router> */}
